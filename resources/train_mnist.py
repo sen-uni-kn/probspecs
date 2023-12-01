6b3d57bb53578024115f01dd6f56f7f4379bfe82
cherrywoods <david.boetius@uni-konstanz.de>
2023-12-01 22:32:49 +0100

    MNIST test networks

---
+++ resources/train_mnist.py
@@ -0,0 +1,61 @@
+# Copyright (c) 2023 David Boetius
+# Licensed under the MIT license
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision.datasets import MNIST
+from torchvision.transforms import ToTensor
+
+if __name__ == "__main__":
+    # Small net for tests
+    torch.manual_seed(177878981064172)
+    net = nn.Sequential(
+        nn.Conv2d(1, 2, kernel_size=3, stride=3),
+        nn.ReLU(),
+        nn.Flatten(),
+        nn.Linear(9 * 9 * 2, 10),
+    )
+    optim = torch.optim.Adam(net.parameters())
+    epochs = 10
+    batch_size = 64
+    large_batch_size = 1024
+
+    train_set = MNIST("../.datasets", transform=ToTensor(), download=True)
+    test_set = MNIST("../.datasets", train=False, transform=ToTensor(), download=True)
+    train_loader = DataLoader(train_set, batch_size, shuffle=True)
+    test_loader = DataLoader(train_set, large_batch_size)
+
+    loss_func = nn.CrossEntropyLoss()
+
+    def batch_accuracy(inputs, targets):
+        predictions = net(inputs)
+        return (torch.argmax(predictions, dim=1) == targets).float().mean()
+
+    def accuracy(loader):
+        result = 0.0
+        for inputs, targets in loader:
+            result += batch_accuracy(inputs, targets)
+        return result / len(loader)
+
+    log_frequency = len(train_loader) // 10
+    for epoch in range(epochs):
+        for i in range(len(train_loader)):
+            optim.zero_grad()
+
+            inputs, targets = next(iter(train_loader))
+            predictions = net(inputs)
+            loss = loss_func(predictions, targets)
+            loss.backward()
+            optim.step()
+
+            if i % log_frequency == 0 or i == len(train_loader) - 1:
+                batch_acc = batch_accuracy(inputs, targets)
+                train_acc = accuracy(train_loader)
+                test_acc = accuracy(test_loader)
+                print(
+                    f"Epoch {epoch:3.0f}/{epochs} ({i/(len(train_loader)-1)*100:3.0f}%), "
+                    f"Batch Loss: {loss:6.4f}, Batch Accuracy: {batch_acc*100:4.1f}%, "
+                    f"Train Accuracy: {train_acc*100:4.1f}%, "
+                    f"Test Accuracy: {test_acc*100:4.1f}%"
+                )
+    torch.save(net, "mnist_network.pyt")
---
+++ resources/train_mnist_gan.py
@@ -0,0 +1,141 @@
+# Copyright (c) 2023 David Boetius
+# Licensed under the MIT license
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from torchvision.datasets import MNIST
+from torchvision.transforms import ToTensor
+import torchvision.utils as vutils
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+    # Train a MNIST WassersteinGAN.
+    # Based on:
+    #  - https://machinelearningmastery.com/how-to-code-a-wasserstein-generative-adversarial-network-wgan-from-scratch/
+    #  - https://pytorch.org/tutorials/beginner/dcgan_faces_tutorial.html
+    # Install tensorboard to view the training progress
+    device = "cuda:0"
+
+    # Small generator for tests
+    torch.manual_seed(659917698158452)
+    generator = nn.Sequential(
+        nn.ConvTranspose2d(4, 49, kernel_size=4, stride=1, bias=False),  # 49 x 4 x 4
+        nn.BatchNorm2d(49, affine=True),
+        nn.LeakyReLU(negative_slope=0.2),
+        nn.ConvTranspose2d(49, 12, kernel_size=4, stride=4, bias=False),  # 12 x 16 x 16
+        nn.BatchNorm2d(12, affine=True),
+        nn.LeakyReLU(negative_slope=0.2),
+        nn.ConvTranspose2d(12, 1, kernel_size=13, stride=1, bias=False),  # 1 x 28 x 28
+        nn.Sigmoid(),
+    )
+    discriminator = nn.Sequential(  # in: 1 x 28 x 28
+        nn.Conv2d(1, 8, kernel_size=4, stride=2, padding=3, bias=False),  # 8 x 16 x 16
+        nn.BatchNorm2d(8),
+        nn.LeakyReLU(negative_slope=0.2, inplace=True),
+        nn.Conv2d(8, 16, kernel_size=4, stride=2, padding=1, bias=False),  # 16 x 8 x 8
+        nn.BatchNorm2d(16),
+        nn.LeakyReLU(negative_slope=0.2, inplace=True),
+        nn.Conv2d(16, 32, kernel_size=4, stride=2, padding=1, bias=False),  # 16 x 4 x 4
+        nn.BatchNorm2d(32),
+        nn.LeakyReLU(negative_slope=0.2, inplace=True),
+        nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1, bias=False),  # 64 x 2 x 2
+        nn.BatchNorm2d(64),
+        nn.LeakyReLU(negative_slope=0.2, inplace=True),
+        nn.Flatten(),  # 256
+        nn.Linear(256, 128),
+        nn.LeakyReLU(negative_slope=0.2, inplace=True),
+        nn.Linear(128, 2),
+    )
+    generator = generator.to(device)
+    discriminator = discriminator.to(device)
+    gen_optim = torch.optim.Adam(generator.parameters(), lr=0.001)
+    gen_lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(
+        gen_optim, [10, 40]  # epochs
+    )
+    disc_optim = torch.optim.Adam(discriminator.parameters(), lr=0.001)
+    disc_lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(disc_optim, [5, 25])
+    epochs = 60
+    discriminator_updates = 3
+    batch_size = 512
+    disc_label_noise = 0.1
+
+    real_data = MNIST("../.datasets", transform=ToTensor(), download=True)
+    real_loader = DataLoader(real_data, batch_size, shuffle=True)
+
+    ce_loss = nn.CrossEntropyLoss()
+
+    # for evaluating training progress
+    fixed_noise = torch.randn((32, 4, 1, 1), device=device)
+    tensorboard = SummaryWriter("../.tensorboard")
+    fake_data = generator(fixed_noise).detach().cpu()
+    image_grid = vutils.make_grid(fake_data, nrow=8)
+    tensorboard.add_image("generated images", image_grid, global_step=0)
+
+    num_iters = len(real_loader) // discriminator_updates
+    log_frequency = num_iters // 10
+    label = torch.empty((batch_size,), dtype=torch.long, device=device)
+    label_flip_prob = torch.full((batch_size,), disc_label_noise, device=device)
+    for epoch in range(epochs):
+        for i in range(num_iters):
+
+            disc_error_real = disc_error_fake = 1.0
+            for i_disc in range(discriminator_updates):
+                disc_optim.zero_grad()
+                real_data, _ = next(iter(real_loader))
+                real_data = real_data.to(device)
+                label = 1 - torch.bernoulli(label_flip_prob).long()
+
+                output = discriminator(real_data)
+                disc_error_real = ce_loss(output, label)
+                disc_error_real.backward()
+
+                with torch.no_grad():
+                    noise = torch.randn((batch_size, 4, 1, 1), device=device)
+                    fake_data = generator(noise)
+                    label = torch.bernoulli(label_flip_prob).long()
+
+                output = discriminator(fake_data)
+                disc_error_fake = ce_loss(output, label)
+                disc_error_fake.backward()  # accumulates gradients
+
+                disc_optim.step()
+                # clip discriminator weights
+                with torch.no_grad():
+                    for layer in discriminator:
+                        if isinstance(layer, (nn.Conv2d, nn.Linear)):
+                            layer.weight.clip(-0.01, 0.01)
+
+            gen_optim.zero_grad()
+            noise = torch.randn((batch_size, 4, 1, 1), device=device)
+            fake_data = generator(noise)
+            label.fill_(1)  # fake data is real for generator loss
+            score = discriminator(fake_data)
+            gen_error = ce_loss(score, label)
+            gen_error.backward()
+            gen_optim.step()
+
+            if i % log_frequency == 0 or i == num_iters - 1:
+                disc_error = (disc_error_real + disc_error_fake) / 2
+                print(
+                    f"Epoch {epoch+1:3.0f}/{epochs} ({i/(num_iters-1)*100:3.0f}%), "
+                    f"Generator Loss: {gen_error:6.4f}, "
+                    f"Discriminator Loss: {disc_error:6.4f} "
+                    f"({disc_error_real:6.4f}/{disc_error_fake:6.4f}) (real/fake)"
+                )
+                step = epoch * num_iters + i + 1
+                tensorboard.add_scalar("generator loss", gen_error, global_step=step)
+                tensorboard.add_scalar(
+                    "discriminator loss", disc_error, global_step=step
+                )
+                with torch.no_grad():
+                    fake_data = generator(fixed_noise).detach().cpu()
+                    image_grid = vutils.make_grid(fake_data, nrow=8)
+                tensorboard.add_image("generated images", image_grid, global_step=step)
+                tensorboard.flush()
+        gen_lr_scheduler.step()
+        disc_lr_scheduler.step()
+
+    torch.save(generator.to("cpu"), "mnist_generator.pyt")
+    torch.save(discriminator.to("cpu"), "mnist_discriminator.pyt")
