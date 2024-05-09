f5f41f273d7de87efb045208db9c66ce501ded39
David Boetius <david.boetius@uni-konstanz.de>
2024-05-09 11:00:10 +0200

    Fix run scripts

--- experiments/acasxu/run.sh
+++ experiments/acasxu/run.sh
@@ -14,21 +14,21 @@ TIMEOUT=900
 PRECISION=0.01
 for net in "2_1" "2_2" "2_3" "2_4" "2_5" "2_6" "2_7" "2_8" "2_9"
 do
-  python "$HERE/safety.py" -u \
+  python -u "$HERE/safety.py" \
   --network "$net" --property 2 --timeout "$TIMEOUT" --precision "$PRECISION" \
   "$@" \
   | tee "$OUT_DIR/${net}_property2.txt"
 done
 for net in "1_9" "net_1_9_property_7_partially_repaired_1" "net_1_9_property_7_partially_repaired_2" "net_1_9_property_7_partially_repaired_3"
 do
-  python "$HERE/safety.py" -u \
+  python -u "$HERE/safety.py" \
   --network "$net" --property 7 --timeout "$TIMEOUT" --precision "$PRECISION" \
   "$@" \
   | tee "$OUT_DIR/${net}_property7.txt"
 done
 for net in "2_9" "net_2_9_property_8_unknown"
 do
-  python "$HERE/safety.py" -u \
+  python -u "$HERE/safety.py" \
   --network "$net" --property 8 --timeout "$TIMEOUT" --precision "$PRECISION" \
   "$@" \
   | tee "$OUT_DIR/${net}_property8.txt"
@@ -45,7 +45,7 @@ do
   do
     for i in {0..5}
     do
-      python "$HERE/robustness.py" -u \
+      python -u "$HERE/robustness.py" \
       --network "1_1" --label "$label" --target "$target" --input "$i" \
       --timeout "$TIMEOUT" --precision "$PRECISION" \
       "$@" \
--- experiments/group_fairness_tabular/run.sh
+++ experiments/group_fairness_tabular/run.sh
@@ -19,7 +19,7 @@ timeout 4h python -u "$HERE/verify.py" \
   --network tiny_network \
   --disadvantaged-group Female \
   --advantaged-group Male \
-  $@ \
+  "$@" \
   | tee "$OUT_DIR/demographic_parity_female_male.log"
 
 timeout 4h python -u "$HERE/verify.py" \
@@ -29,7 +29,7 @@ timeout 4h python -u "$HERE/verify.py" \
   --network tiny_network \
   --disadvantaged-group Black \
   --advantaged-group White \
-  $@ \
+  "$@" \
   | tee "$OUT_DIR/demographic_parity_black_white.log"
 
 timeout 4h python -u "$HERE/verify.py" \
@@ -39,7 +39,7 @@ timeout 4h python -u "$HERE/verify.py" \
   --network tiny_network \
   --disadvantaged-group Asian-Pac-Islander \
   --advantaged-group White \
-  $@ \
+  "$@" \
   | tee "$OUT_DIR/demographic_parity_asian_pac_islander_white.log"
 
 timeout 4h python -u "$HERE/verify.py" \
@@ -49,7 +49,7 @@ timeout 4h python -u "$HERE/verify.py" \
   --network tiny_network \
   --disadvantaged-group Amer-Indian-Eskimo \
   --advantaged-group White \
-  $@ \
+  "$@" \
   | tee "$OUT_DIR/demographic_parity_amer_indian_eskimo_white.log"
 
 timeout 4h python -u "$HERE/verify.py" \
@@ -59,7 +59,7 @@ timeout 4h python -u "$HERE/verify.py" \
   --network tiny_network \
   --disadvantaged-group Race-Other \
   --advantaged-group White \
-  $@ \
+  "$@" \
   | tee "$OUT_DIR/demographic_parity_other_white.log"
 
 timeout 4h python -u "$HERE/verify.py" \
@@ -69,7 +69,7 @@ timeout 4h python -u "$HERE/verify.py" \
   --network tiny_network \
   --disadvantaged-group Non-White \
   --advantaged-group White \
-  $@ \
+  "$@" \
   | tee "$OUT_DIR/demographic_parity_non_white_white.log"
 
 timeout 4h python -u "$HERE/verify.py" \
@@ -79,7 +79,7 @@ timeout 4h python -u "$HERE/verify.py" \
   --network tiny_network \
   --disadvantaged-group Own-child \
   --advantaged-group Married \
-  $@ \
+  "$@" \
   | tee "$OUT_DIR/demographic_parity_own_child_married.log"
 
 timeout 4h python -u "$HERE/verify.py" \
@@ -89,7 +89,7 @@ timeout 4h python -u "$HERE/verify.py" \
   --network tiny_network \
   --disadvantaged-group Unmarried \
   --advantaged-group Married \
-  $@ \
+  "$@" \
   | tee "$OUT_DIR/_demographic_parity_unmarried_married.log"
 
 echo "SouthGerman"
@@ -104,7 +104,7 @@ timeout 12h python -u "$HERE/verify.py" \
   --network tiny_network \
   --disadvantaged-group Female \
   --advantaged-group Male \
-  $@ \
+  "$@" \
   | tee "$OUT_DIR/demographic_parity_female_male.log"
 
 timeout 4h python -u "$HERE/verify.py" \
@@ -114,6 +114,6 @@ timeout 4h python -u "$HERE/verify.py" \
   --network tiny_network \
   --disadvantaged-group Foreign-Worker \
   --advantaged-group Non-Foreign-Worker \
-  $@ \
+  "$@" \
   | tee "$OUT_DIR/demographic_parity_foreign_worker.log"
 
