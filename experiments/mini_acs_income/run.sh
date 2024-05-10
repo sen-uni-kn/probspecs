859d6f3bb9f625b97c4427cd5c5fca7b8700fbed
David Boetius <david.boetius@uni-konstanz.de>
2024-05-10 19:53:35 +0200

    experiment on different network sizes

--- experiments/mini_acs_income/run.sh
+++ experiments/mini_acs_income/run.sh
@@ -22,6 +22,29 @@ do
   | tee "$OUT_DIR/${nv}_variables.log"
 done
 
+# Networks of different sizes
+for size in {1000..10000..1000}
+do
+  timeout "$HARD_TIMEOUT" python -u "$HERE/verify.py" \
+  --num-variables 4 \
+  --timeout "$TIMEOUT" \
+  --network "size_${size}"
+  "$@" \
+  | tee "$OUT_DIR/4_variables_${size}_neurons.log"
+done
+
+# Networks of different depths
+for depth in {2..10}
+do
+  timeout "$HARD_TIMEOUT" python -u "$HERE/verify.py" \
+  --num-variables 4 \
+  --timeout "$TIMEOUT" \
+  --network "depth_${depth}"
+  "$@" \
+  | tee "$OUT_DIR/4_variables_${depth}_layers.log"
+done
+
+# Enumeration approach
 OUT_DIR="$HERE/../output/$TIMESTAMP/mini_acs_income/enumerate"
 mkdir -p "$OUT_DIR"
 
@@ -33,3 +56,4 @@ do
   "$@" \
   | tee "$OUT_DIR/${nv}_variables.log"
 done
+
--- train_networks.sh
+++ train_networks.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
-echo "Training MiniACSIncome networks"
-for i in {1..8}
+# echo "Training MiniACSIncome networks"
+# for i in {1..8}
+# do
+#   NET_NAME="MiniACSIncome-${i}_network"
+#   python experiments/train_on_tabular_datasets.py \
+#     --dataset "MiniACSIncome-$i" \
+#     --out "resources/MiniACSIncome/${NET_NAME}.pyt" \
+#     --use-hyperparameters "resources/MiniACSIncome/${NET_NAME}_hyperparameters.yaml"
+# done
+
+# vary network size
+# for size in 20 30 40 50 60 70 80 90 100 200 300 400 500 600 700 800 900 1000 2000 3000 4000 5000 6000 7000 8000 9000 10000
+# do
+#   NET_NAME="MiniACSIncome-4_size_${size}"
+#   python experiments/train_on_tabular_datasets.py \
+#     --dataset "MiniACSIncome-4" \
+#     --out "resources/MiniACSIncome/${NET_NAME}.pyt" \
+#     --use-hyperparameters "resources/MiniACSIncome/MiniACSIncome-4_size_hyperparameters.yaml" \
+#     --architecture "$size"
+# done
+
+for depth in {2..10}
 do
-  NET_NAME="MiniACSIncome-${i}_network"
+  NET_NAME="MiniACSIncome-4_depth_${depth}"
   python experiments/train_on_tabular_datasets.py \
-    --dataset "MiniACSIncome-$i" \
+    --dataset "MiniACSIncome-4" \
     --out "resources/MiniACSIncome/${NET_NAME}.pyt" \
-    --use-hyperparameters "resources/MiniACSIncome/${NET_NAME}_hyperparameters.yaml"
+    --use-hyperparameters "resources/MiniACSIncome/MiniACSIncome-4_size_hyperparameters.yaml" \
+    --architecture $(printf '10 %.0s' $(seq 1 "$depth"))
 done
