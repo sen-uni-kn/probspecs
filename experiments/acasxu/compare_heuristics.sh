dd48a9bb533dd150aba2f5946553d7122fcdb7bf
David Boetius <david.boetius@uni-konstanz.de>
2024-05-17 18:39:44 +0200

    remove acas xu safety from compare heuristics

--- experiments/acasxu/compare_heuristics.sh
+++ experiments/acasxu/compare_heuristics.sh
@@ -7,20 +7,6 @@ then
   TIMESTAMP="$(date -u +%Y-%m-%d_%H-%M-%S)"
 fi
 
-TIMEOUT=60
-OUT_DIR="$HERE/../output/$TIMESTAMP/acasxu/safety/$TIMEOUT"
-mkdir -p "$OUT_DIR"
-for i1 in {2..5}
-do
-  for i2 in {1..9}
-  do
-    python -u "$HERE/safety.py" \
-    --network "${i1}_${i2}" --property 2 --timeout "$TIMEOUT" \
-    "$@" \
-    | tee "$OUT_DIR/property2_${i1}_${i2}.log"
-  done
-done
-
 OUT_DIR="$HERE/../output/$TIMESTAMP/acasxu/robustness"
 mkdir -p "$OUT_DIR"
 
