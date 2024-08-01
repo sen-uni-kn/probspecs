c47aa0782907055063842bc64c635a1835c27b2b
David Boetius <david.boetius@uni-konstanz.de>
2024-08-01 22:34:22 +0200

    MiniACSIncome with random heuristics experiment

---
+++ experiments/mini_acs_income/run_additional.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+HERE="$(dirname "$0")"
+
+if [ -z ${TIMESTAMP+x} ];
+then
+  TIMESTAMP="$(date -u +%Y-%m-%d_%H-%M-%S)"
+fi
+
+OUT_DIR="$HERE/../output/$TIMESTAMP/mini_acs_income/verify"
+mkdir -p "$OUT_DIR"
+
+TIMEOUT=3600
+HARD_TIMEOUT=3660s  # 60 seconds for setup, etc (very generous)
+
+# Networks of different sizes
+for size in {1000..10000..1000}
+do
+  timeout "$HARD_TIMEOUT" python -u "$HERE/verify.py" \
+  --num-variables 4 \
+  --timeout "$TIMEOUT" \
+  --network "size_${size}" \
+  "$@" \
+  | tee "$OUT_DIR/4_variables_${size}_neurons.log"
+done
+
+# Networks of different depths
+for depth in {2..10}
+do
+  timeout "$HARD_TIMEOUT" python -u "$HERE/verify.py" \
+  --num-variables 4 \
+  --timeout "$TIMEOUT" \
+  --network "depth_${depth}" \
+  "$@" \
+  | tee "$OUT_DIR/4_variables_${depth}_layers.log"
+done
