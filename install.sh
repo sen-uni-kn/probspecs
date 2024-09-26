d765f6bd5255f4a05866f24e299315d178235596
David Boetius <david.boetius@uni-konstanz.de>
2024-09-26 17:43:55 +0200

    Add packaging setup

--- install.sh
+++ install.sh
@@ -2,14 +2,37 @@
 
 set -e  # fail script if any command fails
 
-conda env create -f environment.yaml
-conda activate prob-specs
-conda env config vars set PYTHONPATH="$(pwd)"
-conda activate prob-specs
-
-# Pull and Install auto_LiRPA
+# Pull and install auto_LiRPA and other dependencies
 git submodule update --init
-cd auto_LiRPA
-python setup.py install
-cd ..
 
+conda create -y -n "probspecs" python=3.10 pip
+conda activate probspecs
+
+# Install PyTorch, optionally with CUDA support
+if [ -f "/proc/driver/nvidia/version" ]; then  # CUDA installed?
+    if [ -z "$CUDA_VERSION" ]; then
+        if [ -x "$(command -v nvcc)" ]; then  # Note: this only works with the nvidia-cuda-toolkit installed
+            CUDA_VERSION=$(nvcc --version | grep release | sed 's/.*release //' | sed 's/,.*//')
+        else
+            CUDA_VERSION="10.2"
+        fi
+    fi
+
+    # Map to the three supported CUDA version for pytorch 1.12.1
+    if (( $(echo "$CUDA_VERSION>=11.6" | bc -l) )); then
+        CUDA_VERSION="11.6"
+    elif (( $(echo "$CUDA_VERSION>=11.3" | bc -l) )); then
+        CUDA_VERSION="11.3"
+    else
+        CUDA_VERSION="10.2"
+    fi
+
+    echo "Installing PyTorch with CUDA version $CUDA_VERSION support."
+    conda install -y pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit="$CUDA_VERSION" -c pytorch -c conda-forge
+else
+    echo "Installing PyTorch without CUDA support."
+    conda install -y pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cpuonly -c pytorch
+fi
+
+# Install probspecs
+pip install -e .
--- probspecs/__init__.py
+++ probspecs/__init__.py
@@ -1,5 +1,9 @@
 # Copyright (c) 2023 David Boetius
 # Licensed under the MIT license
+"""Probabilistic Specifcation Verification of Neural Networks"""
+
+__version__ = "0.1.0"
+
 from .formula import *
 from .input_space import InputSpace, TabularInputSpace, TensorInputSpace
 from .trinary_logic import TrinaryLogic
